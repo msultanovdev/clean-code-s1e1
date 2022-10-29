8f32ae797dea7c04ef07b66919bbd9ec98e0bcd8
msultanovdev <mukhammadamin.sultanov@gmail.com>
2022-10-29 09:29:59 +0500

    refactor: adjust HTML-formatting according to rule 3.8 in index.html | app.js | style.css

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-//Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
-
-//Problem: User interaction does not provide the correct results.
-//Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
-
-
-// Event handling, user interaction is what starts the code execution.
-
 var taskInput=document.getElementById("newTask");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
